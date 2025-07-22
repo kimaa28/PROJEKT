42ebdc8358ef220817aca7b13d5f180705ad985a
kimaa28 <kitiojordan28@gmail.com>
2025-07-22 21:14:05 +0200

    login

--- GUI/login.py
+++ GUI/login.py
@@ -1,45 +1,15 @@
 import customtkinter as ctk
 
 
-def start_move(event):
-    app.x = event.x
-    app.y = event.y
 
-def do_move(event):
-    x = app.winfo_x() - app.x + event.x
-    y = app.winfo_y() - app.y + event.y
-    app.geometry(f"+{x}+{y}")
-
-# login.py
-# This script creates a custom login window using customtkinter.
-# It includes a title bar, username and password fields, and buttons for login and account creation.
-
-# Initialize the customtkinter application
 
 app = ctk.CTk()
 app.geometry("400x300")
 app.title("Login Window")
-app.overrideredirect(True)  # Remove window decorations
-app.after(10, lambda: app.focus_force())  # Set focus to the window
-app.attributes("-topmost", True)  # Keep the window on top
-app.attributes("-alpha", 0.95)  # Set transparency
-app.resizable(False, False)  # Disable resizing
 app.geometry("700x500")  # Set initial size
 
-# Create a custom title bar
-over_frame = ctk.CTkFrame(app, corner_radius=0, bg_color="black",fg_color="#111", height=30)
-over_frame.pack(fill="x", side="top")
-app_title = ctk.CTkLabel(over_frame, text="Login", bg_color="#111", text_color="white", font=("Arial", 16))
-app_title.pack(side="left", padx=10)
-close_button = ctk.CTkButton(over_frame, text="X", command=app.destroy, width=30, height=30, fg_color="red", corner_radius=3, hover_color="darkred")
-close_button.pack(side="right", padx=5, pady=5)
-scale_button = ctk.CTkButton(over_frame, text="◻", command=lambda: ctk.set_window_scaling(2), width=30, height=30, corner_radius=3)
-scale_button.pack(side="right", pady=5)
 
-over_frame.bind("<Button-1>", start_move)
-over_frame.bind("<B1-Motion>", do_move)
-app_title.bind("<Button-1>", start_move)
-app_title.bind("<B1-Motion>", do_move)
+
 
 longin_frame = ctk.CTkFrame(app, corner_radius=20, fg_color="#333", width=600, height=600)
 longin_frame.pack(expand=True, padx=20, pady=20)
